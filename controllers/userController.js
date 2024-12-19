daee5344b6d3785e4f3106bcce6ab30c7e6ffe01
yashshivasyndell <yash.s@syndelltech.in>
2024-12-19 18:20:42 +0530

    homonym

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const allUser = require('../models/allUser')
 
 const getData = async (req,res)=>{
     try{
-        const users = await allUser.find().select("email phoneNumber username dateofbirth gender education country role created picture");
+        const users = await allUser.find().select("email phoneNumber username dateofbirth gender education country role created picture verification isGuest");
         return res.status(200).json({
             success:true,
             data:users
